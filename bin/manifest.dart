aaf0f14b5917e6dc95632931d90c53979b06a791
James Aprosail <aprosail@outlook.com>
2024-06-27 08:15:39 +0800

    template dart bin

---
+++ bin/manifest.dart
@@ -0,0 +1,5 @@
+import 'package:manifest/manifest.dart' as manifest;
+
+void main(List<String> arguments) {
+  print('Hello world: ${manifest.calculate()}!');
+}
---
+++ test/manifest_test.dart
@@ -0,0 +1,8 @@
+import 'package:manifest/manifest.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
