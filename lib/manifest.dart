782a9bd63ef56281ff42f86927bbe873c09f1fcb
James Aprosail <aprosail@outlook.com>
2024-06-27 10:19:21 +0800

    parse manifest from yaml

--- lib/manifest.dart
+++ lib/manifest.dart
@@ -1,1 +1,79 @@
+import 'package:yaml/yaml.dart';
 
+class Manifest {
+  const Manifest({
+    required this.name,
+    required this.version,
+    this.description,
+    this.homepage,
+  });
+
+  final String name;
+  final String version;
+  final String? description;
+  final String? homepage;
+
+  static Manifest fromYaml(String raw) {
+    final yaml = loadYaml(raw) as Map;
+    return Manifest(
+      name: yaml.check(['name']),
+      version: yaml.check(['version']),
+      description: yaml.peek(['description']),
+      homepage: yaml.peek(['homepage']),
+    );
+  }
+}
+
+extension MapParser on Map<Object?, Object?> {
+  /// Parse a value from a map with given path.
+  ///
+  /// 1. The returned value is the value when found or error.
+  /// 2. The returned depth is the index of the path when return (start from 0).
+  /// 3. If success, the depth should be the same as [path]'s length.
+  (Object? value, int depth) parse<T>(List<Object?> path) {
+    Object? current = this;
+    for (int i = 0; i < path.length; i++) {
+      final key = path[i];
+      if (!(current is Map && current.containsKey(key))) return (null, i);
+      current = current[key];
+      if (i != path.length - 1) continue;
+      return current is T ? (current, path.length) : (current, i);
+    }
+    return (null, -1); // Unreachable.
+  }
+
+  /// Encapsulation of [parse],
+  /// throw error when cannot parse the value.
+  T check<T>(List<Object?> path) {
+    final (value, depth) = parse<T>(path);
+    if (depth != path.length) throw InvalidYamlStructure(path, depth, value);
+    return value as T;
+  }
+
+  /// Encapsulation of [parse],
+  /// return null when cannot parse the value.
+  T? peek<T>(List<Object?> path) {
+    final (value, depth) = parse<T>(path);
+    return depth == path.length ? value as T : null;
+  }
+}
+
+class InvalidYamlStructure extends Error {
+  /// See [toString] to understand what its format means.
+  InvalidYamlStructure(this.path, this.depth, this.value);
+
+  final List<Object?> path;
+  final int depth;
+  final Object? value;
+
+  /// The error format will output its path and value.
+  /// 1. The path is the current path when error happens.
+  /// 2. When the value is correct, it means the specified type might be error.
+  /// 3. Generics will not be available after compilation,
+  ///    so it cannot output the specified type here.
+  @override
+  String toString() {
+    final path = this.path.sublist(0, depth + 1).join('.');
+    return 'InvalidYamlStructure: $path: $value';
+  }
+}
--- test/manifest_test.dart
+++ test/manifest_test.dart
@@ -1,7 +1,79 @@
+import 'package:manifest/manifest.dart';
 import 'package:test/test.dart';
+import 'package:yaml/yaml.dart';
 
 void main() {
-  test('placeholder', () {
-    expect(1 + 1, 2);
+  const raw = '''
+    name: example
+    description: This is an example.
+    version: 1.2.3 # Here is comments.
+    publish_to: "none" # Use double quote here.
+    environment: {sdk: '>=3.4.3 <4.0.0'}
+  ''';
+
+  group('parse yaml', () {
+    final yaml = loadYaml(raw);
+    test('load yaml type', () => expect(yaml is Map, true));
+
+    final map = yaml as Map;
+
+    group('parse', () {
+      test('not exist', () => expect(map.parse<int>(['not-exist']), (null, 0)));
+      test('wrong type', () {
+        expect(map.parse<int>(['name']), ('example', 0));
+      });
+
+      test('normal single layer', () {
+        expect(map.parse<String>(['name']), ('example', 1));
+        expect(map.parse<String>(['version']), ('1.2.3', 1));
+        expect(map.parse<String>(['publish_to']), ('none', 1));
+      });
+
+      test('nesting', () {
+        expect(
+          map.parse<String>(['environment', 'sdk']),
+          ('>=3.4.3 <4.0.0', 2),
+        );
+      });
+    });
+
+    group('check', () {
+      test('success', () {
+        expect(map.check<String>(['name']), 'example');
+        expect(map.check<String>(['version']), '1.2.3');
+        expect(map.check<String>(['publish_to']), 'none');
+        expect(map.check<String>(['environment', 'sdk']), '>=3.4.3 <4.0.0');
+      });
+
+      test('error message: cannot find', () {
+        try {
+          map.check<String>(['not-exist']);
+        } on InvalidYamlStructure catch (e) {
+          expect(e.toString(), 'InvalidYamlStructure: not-exist: null');
+        }
+      });
+
+      test('error message: error type', () {
+        try {
+          map.check<int>(['name']);
+        } on InvalidYamlStructure catch (e) {
+          expect(e.toString(), 'InvalidYamlStructure: name: example');
+        }
+      });
+    });
+
+    test('peek', () {
+      expect(map.peek<int>(['name']), null);
+      expect(map.peek<String>(['not-exist']), null);
+      expect(map.peek<String>(['name']), 'example');
+    });
+  });
+
+  test('parse manifest', () {
+    final manifest = Manifest.fromYaml(raw);
+    expect(manifest.name, 'example');
+    expect(manifest.description, 'This is an example.');
+    expect(manifest.version, '1.2.3');
+    expect(manifest.homepage, null);
   });
 }
